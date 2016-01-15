0eab1b5cbaca30e8371b9dbe41a501968c9f9370
girish@cloudron.io <girish@cloudron.io>
2016-01-14 16:36:10 -0800

    Implement generic OAuth login
    This adds the following config:
        "OAuthSettings": {
            "Enable": false,
            "ProviderName": "",  // like 'cloudron', 'qt', 'google', 'gitlab'
            "Secret": "",        // client secret
            "Id": "",            // client id
            "Scope": "",         // oauth scope
            "AuthEndpoint": "",  // auth endpoint
            "TokenEndpoint": "", // token endpoint
            "UserApiEndpoint": "", // user profile end point
            "UsernameField": "",  // the field in user profile to be used as username
            "EmailField": "",    // the field in user profile to be used as email
            "AuthDataField": ""  // the field in user profile to be used as auth data
        },
    
    The above values can be configured via the admin panel.
    
    We should be able to merge the gitlab oauth code with this code as well.
    
    Done with Simon Hausmann <simon.hausmann@theqtcompany.com>

--- web/react/components/signup_user_complete.jsx
+++ web/react/components/signup_user_complete.jsx
@@ -283,6 +283,18 @@ class SignupUserComplete extends React.Component {
            );
         }
 
+        if (global.window.mm_config.EnableSignUpWithOAuth === 'true') {
+            signupMessage.push(
+                <a
+                    className='btn btn-custom-login oauth'
+                    href={'/' + this.props.teamName + '/signup/oauth' + window.location.search}
+                >
+                    <span className='icon'/>
+                    <span>{'Login with OAuth'}</span>
+                </a>
+           );
+        }
+
         var emailSignup;
         if (global.window.mm_config.EnableSignUpWithEmail === 'true') {
             emailSignup = (
